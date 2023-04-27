292d50905078df2e8a3f4521ad424da06bb6bc99
ss6022 <soaibsh4@gmail.com>
2023-04-27 09:30:15 +0530

    final expanded

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,62 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Row(
+          child: Column(
                 children: [
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        color: Colors.blueAccent,
-                         child: Text('1'),
-                      ),
-                  Expanded(
-                    flex: 7,
-                        child: Container(
-                           padding: EdgeInsets.all(20),
-                          color: Colors.greenAccent,
-                          child: Text('2'),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                          padding: EdgeInsets.all(20),
-                          color: Colors.blueGrey,
-                          child: Text('3'),
-                        ),
-                      )
-                ],
+                  _cutomeContainer(number: 8,color: Colors.red),
+                 Expanded(
+                     flex:5,
+                     child: Row(
+                       children:[
+                       _cutomeContainer(number: 5,color: Colors.indigo),
+                         Expanded(
+                         flex:3,
+                         child: Column(
+                         children:[
+                            Expanded(
+                            flex:2,
+                            child: Row(
+                            children:[
+                                    Expanded(
+                                    flex:1,
+                                     child: Column(
+                                     children:[
+                                       _cutomeContainer(number: 1,color: Colors.green),
+                                      _cutomeContainer(number: 1,color: Colors.lightBlue),
+                                     ]
+                                     )),
+                              _cutomeContainer(number: 2,color: Colors.brown),
+                               ]
+                               )),
+                        _cutomeContainer(number: 3,color: Colors.purple),
+                          ],
+                          )
+                         )
+                       ],
+                 )
+                 )
+                 ],
           ),
         ),
       ),
     );
   }
 }
+Widget _cutomeContainer({
+  required int number,
+  required Color color
+}){
+  return  Expanded(
+    flex: number,
+    child: Container(
+      constraints: BoxConstraints.expand(),
+      color: color,
+      child: Center(child: Text('${number}',style: TextStyle(
+          color: Colors.white,
+          fontSize: 30 ),
+        )
+      ),
+    ),
+  );
+}
+
